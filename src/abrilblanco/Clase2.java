1ccf2c325eb2ec917ffeb2d454fb6d7a90bda9eb
dam <dam@203P20.mshome.net>
2025-04-10 11:28:02 +0200

    Merge Final
    Conflicts:
    	src/control/versiones/Segunda.java

---
+++ src/abrilblanco/Clase2.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package abrilblanco;
+
+/**
+ *
+ * @author dam
+ */
+public class Clase2 {
+    String nombre;
+    String rama;
+     public void ramaActual(){
+         rama="PRODUCCION";
+         nombre="Ander";
+        System.out.println(nombre + rama);
+     
+    }
+}
